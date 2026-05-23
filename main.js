3ac932977dba4f70d6bd6aad5fe8f4dfbbd752d2
cochanae-del <co.chanae@gmail.com>
2026-05-23 11:56:07 +0000

    Calculate the area with variables.

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-var height = prompt("Enter the height", 150);
-var width = prompt("Enter the width", 230);
+//var height = prompt("Enter the height", 150);
+//var width = prompt("Enter the width", 230);
     
 // Write your code below
+let height = 150
+let width = 230
+var area = height * width;
 
 
 
 
 // Print out result in the console log statement below 
-console.log();
+console.log(area);
